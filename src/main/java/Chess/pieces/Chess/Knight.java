a23769da9cbd9965d57483c69616f2fddc2e0dd1
Kyle Collins <65318126+Budderman18@users.noreply.github.com>
2023-03-17 16:05:35 -0400

    (almost) complete movement checks

---
+++ src/main/java/Chess/pieces/Chess/Knight.java
@@ -0,0 +1,53 @@
+package Chess.pieces.Chess;
+
+import Chess.pieces.InvalidMovementException;
+import Chess.pieces.Piece;
+
+public class Knight extends Piece
+{
+    public boolean color;
+    public Knight (boolean color, int x, int y)
+    {
+        //Calls Piece contructor 
+        super(x,y);
+
+        //Sets image of pawn depending on color passed.
+        String Chess = "Chess"; 
+        if (color == true) {
+            loadImage(Chess, "whiteKnight");
+        }
+        else {
+            loadImage(Chess, "blackKnight");
+        }
+        this.color = color;
+    }
+ 
+    @Override
+    public void move(int x, int y) throws InvalidMovementException {
+        //local vars
+        int originx = this.getPos().x;
+        int originy = this.getPos().y;
+        int totalXMoved = originx - x;
+        int totalYMoved = originy - y;
+        int absXMoved = totalXMoved;
+        int absYMoved = totalYMoved;
+        if (totalXMoved < 0) {
+            absXMoved*=-1;
+        }
+        if (totalYMoved < 0) {
+            absYMoved*=-1;
+        }
+        System.out.println(x + " - " + originx + " = " + absXMoved);
+        System.out.println(y + " - " + originy + " = " + absYMoved);
+        //check if x value changed 
+        if (!(absYMoved == 2 && absXMoved == 1)) {
+            if (!(absYMoved == 1 && absXMoved == 2)) {
+                throw new InvalidMovementException("Knights can only move in increments of 1x, 2y or 2x, 1y");
+            }
+        }
+        //set position
+        this.setPos(originx + x, originy + y);
+    }
+}
+    
+
