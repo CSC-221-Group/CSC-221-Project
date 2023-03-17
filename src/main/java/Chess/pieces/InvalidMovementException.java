eadc6f8ac16dace0a41d1d47a0d1d0c4302e62a7
Kyle Collins <65318126+Budderman18@users.noreply.github.com>
2023-03-17 16:08:46 -0400

    (almost) complete movement checks

---
+++ src/main/java/Chess/pieces/InvalidMovementException.java
@@ -0,0 +1,19 @@
+package Chess.pieces;
+
+/**
+ *
+ * This exception should be thrown when a piece attempts to do an invalid move
+ * 
+ */
+public class InvalidMovementException extends Exception {
+
+    /**
+     * 
+     * This constructor calls a new InvalidMovement
+     *
+     * @param msg the message.
+     */
+    public InvalidMovementException(String msg) {
+        super(msg);
+    }
+}
